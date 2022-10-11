63c29707d658e99c67daebfce597301bca2ae01b
smixale <114954579+smixale@users.noreply.github.com>
2022-10-11 18:41:09 +0200

    controller slider

--- v1/Assets/player.cs
+++ v1/Assets/player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public gestoreVita vita;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +21,25 @@ public class player : MonoBehaviour
             vita.danneggia(1);
         }
 //!-----------------debug-------------------
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("collect")) {
+                Collect(other.GetComponent<collect>());
+        }
+    }
+
+    private void Collect(collect collectable) {
+        if(collectable.Collect()) {
+            if(collectable is collectStellina1) {
+                Debug.Log ("ho preso la stellina 1");
+            }
+            else if(collectable is collectStellina2) {
+                Debug.Log ("ho preso la stellina 2");
+            }
+            else if(collectable is collectStellina3) {
+                Debug.Log ("ho preso la stellina 3");
+            }
+        }
     }
 }
