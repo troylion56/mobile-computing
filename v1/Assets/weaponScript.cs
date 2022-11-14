c3db21cdc5777e5bcbae6ad632c7e350ed25a7f8
ludovica01 <ludovica.marinangeli01@gmail.com>
2022-11-14 15:51:41 +0100

    shootingConBottone
    Modificato lo shooting con bottone, senza caricamento

--- v1/Assets/weaponScript.cs
+++ v1/Assets/weaponScript.cs
@@ -7,23 +7,20 @@ public class weaponScript : MonoBehaviour
 
     public Transform firePoint;
     public GameObject bulletPreFab;
-    public float timeScale;
-    public float spawnTimer;
-
+    
     // Update is called once per frame
     void Update()
     {
-        spawnTimer+=Time.deltaTime;
-        if(spawnTimer>timeScale)
-        {
-            Shoot();
-            spawnTimer=0;
-        }
+        
     }
 
     void Shoot()
     {
         Instantiate(bulletPreFab, firePoint.position, firePoint.rotation);
+    }
+
+    public void onClick (){
+        Shoot();
     }
     
 }
