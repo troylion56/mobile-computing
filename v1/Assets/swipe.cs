ccdfbde54a787b57cca96d5f0a37531140cdb81f
smixale <114954579+smixale@users.noreply.github.com>
2022-11-16 19:32:10 +0100

    animazioni create

--- v1/Assets/swipe.cs
+++ v1/Assets/swipe.cs
@@ -15,10 +15,39 @@ public class swipe : MonoBehaviour
     float [] posizioni=new float[5] {-1.8f, -0.9f, 0.0f, 0.9f,1.8f};
     
     int index=2;
+    public Animator spostamento;   
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            index--;
+            if (index<0)
+            {
+                index=4;
+            }
+            //!posizione.position=new Vector2(posizioni[index],posizione.position.y);
+            spostamento.SetTrigger("vaiSx");
+            Debug.Log("sinistra");
+            //!spostamento.SetTrigger("torna");
+            //!StartCoroutine(swipeSx());
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            index++;
+            if (index>4)
+            {
+                index=0;
+            }
+            //!posizione.position=new Vector2(posizioni[index],posizione.position.y);
+            spostamento.SetTrigger("vaiDx");
+            Debug.Log("destra");
+            //!spostamento.SetTrigger("torna");
+            //!StartCoroutine(swipeDx());
+        }
+
         if (Input.touchCount>0&&Input.GetTouch(0).phase==TouchPhase.Began)
         {
             posIniziale=Input.GetTouch(0).position;
@@ -39,6 +68,7 @@ public class swipe : MonoBehaviour
                         index=4;
                     }
                     posizione.position=new Vector2(posizioni[index],posizione.position.y);
+                    spostamento.SetTrigger("vaiSx");
                     Debug.Log("sinistra");
                     stopTouch=true;
                 }
@@ -50,6 +80,7 @@ public class swipe : MonoBehaviour
                         index=0;
                     }
                     posizione.position=new Vector2(posizioni[index],posizione.position.y);
+                    spostamento.SetTrigger("vaiDx");
                     Debug.Log("destra");
                     stopTouch=true;
                 }
@@ -68,5 +99,18 @@ public class swipe : MonoBehaviour
                 Debug.Log("tap");
             }
         }
+    }
+
+        IEnumerator swipeDx(){
+        spostamento.SetTrigger("vaiDx");
+        yield return new WaitForSeconds(0.20f);
+        //!spostamento.SetTrigger("torna");
+    }
+
+    
+    IEnumerator swipeSx(){
+        spostamento.SetTrigger("vaiSx");
+        yield return new WaitForSeconds(0.20f);
+        //!spostamento.SetTrigger("torna");
     }
 }
