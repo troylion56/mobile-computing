3d7fae36a7ddc771982fa0f08cc3993fa59bf352
smixale <114954579+smixale@users.noreply.github.com>
2022-11-30 09:55:57 +0100

    collezionabile
    prima implementazione del collezionabile

--- v1/Assets/collectableBenzina.cs
+++ v1/Assets/collectableBenzina.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class collectableBenzina : collect
 {
- 
+    public gestoreBenzina script;
+    private void OnTriggerEnter2D(Collider2D other) {
+        script.carica(100);
+    }
 }
--- v1/Assets/gestoreBenzina.cs
+++ v1/Assets/gestoreBenzina.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class gestoreBenzina : MonoBehaviour
 {
     public Slider benzina;
-    // Start is called before the first frame update
     void Start()
     {
         benzina.value=100;
