2e1d1ed683f8fdc03c2b97d5e1b705633e547e10
troylion56 <bluphoenix56@gmail.com>
2022-12-01 15:12:31 +0100

    fine con pulsante reset
    aggiornamenti ora prende le stelle e le salva in memoria su tutte e tre i livelli e ci sta il pulsante reset

--- v1/Assets/scripts/gameController.cs
+++ v1/Assets/scripts/gameController.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class gameController : MonoBehaviour{
     public static bool pausa;
-    
+
+
     public static bool s1;
     public static bool s2;
     public static bool s3;
@@ -14,7 +15,12 @@ public class gameController : MonoBehaviour{
     {   
     }
     void Update()
-    {   
+    {  
+        if(Input.GetKeyDown(KeyCode.F)) {
+            fineLivello();
+            Debug.Log("Fine livello");
+        }
+
     }
 
     public void fineLivello (){
@@ -31,26 +37,26 @@ public class gameController : MonoBehaviour{
         }
 
         if(SceneManager.GetActiveScene().name=="SecondoLivello") {
-            if (PlayerPrefs.GetInt("superStella1:1")==0&&s1){
-                PlayerPrefs.SetInt("superStella1:1",1);
+            if (PlayerPrefs.GetInt("superStella2:1")==0&&s1){
+                PlayerPrefs.SetInt("superStella2:1",1);
             }
-            if (PlayerPrefs.GetInt("superStella1:2")==0&&s2){
-                PlayerPrefs.SetInt("superStella1:2",1);
+            if (PlayerPrefs.GetInt("superStella2:2")==0&&s2){
+                PlayerPrefs.SetInt("superStella2:2",1);
             }
-            if (PlayerPrefs.GetInt("superStella1:3")==0&&s3){
-                PlayerPrefs.SetInt("superStella1:3",1);
+            if (PlayerPrefs.GetInt("superStella2:3")==0&&s3){
+                PlayerPrefs.SetInt("superStella2:3",1);
             }
         }
 
         if(SceneManager.GetActiveScene().name=="TerzoLivello") {
-            if (PlayerPrefs.GetInt("superStella1:1")==0&&s1){
-                PlayerPrefs.SetInt("superStella1:1",1);
+            if (PlayerPrefs.GetInt("superStella3:1")==0&&s1){
+                PlayerPrefs.SetInt("superStella3:1",1);
             }
-            if (PlayerPrefs.GetInt("superStella1:2")==0&&s2){
-                PlayerPrefs.SetInt("superStella1:2",1);
+            if (PlayerPrefs.GetInt("superStella3:2")==0&&s2){
+                PlayerPrefs.SetInt("superStella3:2",1);
             }
-            if (PlayerPrefs.GetInt("superStella1:3")==0&&s3){
-                PlayerPrefs.SetInt("superStella1:3",1);
+            if (PlayerPrefs.GetInt("superStella3:3")==0&&s3){
+                PlayerPrefs.SetInt("superStella3:3",1);
             }
         }   
     }
--- v1/Assets/verificaStelleLivello3.cs
+++ v1/Assets/verificaStelleLivello3.cs
@@ -5,38 +5,16 @@ using UnityEngine;
 using System;
 using UnityEngine.UI;
 
-public class verificaStelleLivello : MonoBehaviour
+public class verificaStelleLivello3 : MonoBehaviour
 {
     public SpriteRenderer ss11; //ss prima stellina del primo livello
     public SpriteRenderer ss12; //ss seconda stellina del primo livello
     public SpriteRenderer ss13; //ss terza stellina del primo livello
 
     public Sprite stellaAttiva;
-    void Start(){
-        if (!PlayerPrefs.HasKey("superStella1:1")){
-                PlayerPrefs.SetInt("superStella1:1",0);
-        }
-        else{
-                aggiornaSpirte (PlayerPrefs.GetInt("superStella1:1"),ss11);
-        }
-        if (!PlayerPrefs.HasKey("superStella1:2")){
-                PlayerPrefs.SetInt("superStella1:2",0);
-        }
-        else{
-                aggiornaSpirte (PlayerPrefs.GetInt("superStella1:2"),ss12);
-        }
-        if (!PlayerPrefs.HasKey("superStella1:3")){
-                PlayerPrefs.SetInt("superStella1:3",0);
-        }
-        else{
-                aggiornaSpirte (PlayerPrefs.GetInt("superStella1:3"),ss13);
-        }
-        
-
-
-       
-
-
+    // Start is called before the first frame update
+    void Start()
+    {
         if (!PlayerPrefs.HasKey("superStella3:1")){
                 PlayerPrefs.SetInt("superStella3:1",0);
         }
@@ -55,8 +33,7 @@ public class verificaStelleLivello : MonoBehaviour
         else{
                 aggiornaSpirte (PlayerPrefs.GetInt("superStella3:3"),ss13);
         }
-
-
+        
     }
 
     void aggiornaSpirte(int verifica,SpriteRenderer stella){
@@ -64,5 +41,4 @@ public class verificaStelleLivello : MonoBehaviour
             stella.sprite=stellaAttiva;
         }
     }
-} 
-
+}
