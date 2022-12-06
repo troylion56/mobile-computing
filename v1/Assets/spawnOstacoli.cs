5ca9c16840f0d5d8733aeeaca11510332c3c4aa6
smixale <114954579+smixale@users.noreply.github.com>
2022-12-06 09:03:29 +0100

    implementazione completa
    implementati asteroidi e gestione degli spawn su tutti i livelli

--- v1/Assets/spawnOstacoli.cs
+++ v1/Assets/spawnOstacoli.cs
@@ -9,9 +9,25 @@ public class spawnOstacoli : MonoBehaviour
     public Transform sp3;
     public Transform sp4;
     public Transform sp5;
+
+    public GameObject asteroide;
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.X)){
+            Instantiate(asteroide,sp1.transform.position,sp1.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.C)){
+            Instantiate(asteroide,sp2.transform.position,sp2.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.V)){
+            Instantiate(asteroide,sp3.transform.position,sp3.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.B)){
+            Instantiate(asteroide,sp4.transform.position,sp4.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.N)){
+            Instantiate(asteroide,sp5.transform.position,sp5.rotation);
+        }
     }
 }
