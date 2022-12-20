85811e45091b0ae5bf350e2492436d5ee390593e
smixale <114954579+smixale@users.noreply.github.com>
2022-12-20 20:54:04 +0100

    implementazione
    implementati i collezionabili animati, inseriti tra i preFab e resi istanziabili

--- v1/Assets/collectableBenzina.cs
+++ v1/Assets/collectableBenzina.cs
@@ -7,9 +7,15 @@ public class collectableBenzina : collect
     public gestoreBenzina script;
     Vector3 posizione;
     private void Update() {
-        transform.position = new Vector2 (transform.position.x, transform.position.y-2.5f*Time.deltaTime);
-        posizione += new Vector3(0,2,0);
-        transform.localEulerAngles=posizione;
+        
+        if (!gameController.pausa)
+        {
+            transform.position = new Vector2 (transform.position.x, transform.position.y-2.5f*Time.deltaTime);
+            
+            posizione += new Vector3(0,2,0);
+            transform.localEulerAngles=posizione;  
+        }
+
     }
  //   private void OnTriggerEnter2D(Collider2D other) {
  //       script.carica(100);
