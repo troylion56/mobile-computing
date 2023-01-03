8afc37f708c7b88780d60c587d369bb39e6cd304
troylion56 <bluphoenix56@gmail.com>
2023-01-03 18:20:08 +0100

    merge successo
    damn tony

--- v1/Assets/weaponScriptTutorial.cs
+++ v1/Assets/weaponScriptTutorial.cs
@@ -10,7 +10,7 @@ public class weaponScriptTutorial : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPreFab;
     public static bool stato;
-    public Sprite caricamento0, caricamento1, caricamento2, caricamento3;
+    public Sprite caricamento0, caricamento1, caricamento2, caricamento3,caricamento4;
     public int colpiDisponibili;
     public Button pulsante;
     public Image tasto;
@@ -19,9 +19,13 @@ public class weaponScriptTutorial : MonoBehaviour
     private int contatoreSpari=0;       //variabile che conta gli spari effettuati durante il tutorial dello shooting
     public gestoreTestoTutorial tutorial;
     private bool singolo=true;
+    public SpriteState st;
+    public Sprite onclick1,onclick2,onclick3, onclick4;
     private void Start() {
-       stato = true;
-       colpiDisponibili = 3;            // si inizia con il caricatore pieno
+        stato = true;
+        colpiDisponibili = 4;            // si inizia con il caricatore pieno
+        st.pressedSprite = onclick4;
+        pulsante.spriteState=st;
 
     }
 
@@ -57,12 +61,23 @@ public class weaponScriptTutorial : MonoBehaviour
         if(colpiDisponibili == 1) {
             pulsante.interactable = true;
             tasto.sprite = caricamento1;
+            st.pressedSprite = onclick1;
+            pulsante.spriteState=st;
         }
         else if(colpiDisponibili == 2) {
             tasto.sprite = caricamento2;
+            st.pressedSprite = onclick2;
+            pulsante.spriteState=st;
         }
         else if(colpiDisponibili == 3) {
             tasto.sprite = caricamento3;
+            st.pressedSprite = onclick3;
+            pulsante.spriteState=st;
+        }
+        else if(colpiDisponibili == 4) {
+            tasto.sprite = caricamento4;
+            st.pressedSprite = onclick4;
+            pulsante.spriteState=st;
         }
         else if(colpiDisponibili == 0) {
             tasto.sprite = caricamento0;
