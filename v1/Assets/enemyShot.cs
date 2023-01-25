9dc2bd1e31913dd63c33eb0ce1bc2945b5ae4b2d
ludovica01 <ludovica.marinangeli01@gmail.com>
2023-01-25 19:16:32 +0100

    proiettili ruotano

--- v1/Assets/enemyShot.cs
+++ v1/Assets/enemyShot.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class enemyShot : MonoBehaviour
 {
     Vector3 posizione;
+    float rotateSpeed = 90;
+    Animator animator;
+    public Sprite img1, img2;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-
         transform.position = new Vector2 (transform.position.x, transform.position.y-4f*Time.deltaTime);
-        posizione += new Vector3(0,2,0);
+        posizione += new Vector3(0,2,0);        
 
         if(transform.position.y < -6) {
             Destroy(gameObject);
         }
-        
+
+        float angle = rotateSpeed * Time.deltaTime;
+        transform.rotation *= Quaternion.AngleAxis(angle, Vector3.forward);
+
+
     }
+
+    
 
     private void OnTriggerEnter2D(Collider2D col) {
         if(col.CompareTag("Player")) {
-            Destroy(gameObject);
+            animator.SetTrigger("morto");       // fai scattare la transizione
+            // Destroy(gameObject);
             Debug.Log("Proiettile ha colpito il player ed è stato distrutto");
         }
     }
