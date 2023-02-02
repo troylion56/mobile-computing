15da4cc995a662d95d6f915993ec211d6e437277
troylion56 <bluphoenix56@gmail.com>
2023-02-02 14:39:17 +0100

    login

---
+++ v1/Assets/apriLogin.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class apriLogin : MonoBehaviour
+{
+    public Animator transizione; 
+    public void apri(){
+        transizione.SetTrigger("apriLogin");
+    }
+}
