7637480e77397a1ae6129083bfe9df7df1bedcf7
ludovica01 <ludovica.marinangeli01@gmail.com>
2023-02-13 11:35:29 +0100

    volaAltoFineLvl
    adesso il player quando hai finito il livello vola in alto senza animazioni

--- v1/Assets/scripts/primoLivelloScripts/collezionabili/inventory.cs
+++ v1/Assets/scripts/primoLivelloScripts/collezionabili/inventory.cs
@@ -17,7 +17,20 @@ public class inventory : MonoBehaviour
     public gestoreVita vita;
     public shake cameraShake;
     public Animator player;
+    public Transform posizione;
 
+
+    public void Update() {
+
+        if(gameController.fineLiv && transform.position.y < 7) {
+            transform.position = new Vector2(transform.position.x, transform.position.y + 0.4f);
+            Debug.Log("Sei nella parte dove il player dovrebbe volare in alto perchè hai finito il livello");
+        }
+
+        if(transform.position.y > 7) {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
