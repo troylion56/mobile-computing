3d8d2894e716af225fdab79c744556d297c9163f
troylion56 <bluphoenix56@gmail.com>
2023-03-11 09:47:05 +0100

    rimossoFirebase

--- v1/Assets/saveData.cs
+++ v1/Assets/saveData.cs
@@ -1,36 +1,6 @@
-using Firebase;
-using Firebase.Database;
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
 public class saveData: MonoBehaviour {
-  public DatabaseReference Bdreference;
-  public InputField emailInput;
-
-    private void Start()
-    {
-        // Get the root reference location of the database.
-        Bdreference = FirebaseDatabase.DefaultInstance.RootReference;
-    }
-
-    public void saveDaaa(){
-      StartCoroutine(UpdateUsernameDatabase(emailInput.text));
-    }
-    private IEnumerator UpdateUsernameDatabase(string username)
-    {
-        // Set the currently logged in user username in the database
-        var DBTask = Bdreference.Child("users").Child(username).Child("username").SetValueAsync(username);
-
-        yield return new WaitUntil(() => DBTask.IsCompleted);
-
-        if (DBTask.Exception != null)
-        {
-            Debug.LogWarning($"fallito {DBTask.Exception}");
-        }
-        else
-        {
-            // Database username is now updated
-        }
-    }
 }
