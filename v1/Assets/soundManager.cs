161b8a944487a0a173fa35644b10cc9d249fb132
smixale <114954579+smixale@users.noreply.github.com>
2023-04-19 22:00:00 +0200

    gestione suoni
    implementato il sound controller

---
+++ v1/Assets/soundManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour
+{
+
+    public AudioSource mortePlayer;
+    public AudioSource proiettiliPlayer;
+    public AudioSource missiliPlayer;
+    public AudioSource proiettiliNemici;
+    public AudioSource morteNemici;
+    public AudioSource distruzioneAsteroidi;
+    public AudioSource schiantoAsteroidi;
+    public AudioSource musicaEsterna;
+    public AudioSource musicaLivelli;
+    public AudioSource fineLivello;
+    public AudioSource gameOver;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
