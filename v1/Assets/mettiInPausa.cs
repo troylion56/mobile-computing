733770d24d0b2f6882f9ca0f06b5c76cceb2e189
smixale <114954579+smixale@users.noreply.github.com>
2023-04-30 22:10:33 +0200

    modifiche suoni tutorial

--- v1/Assets/mettiInPausa.cs
+++ v1/Assets/mettiInPausa.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class mettiInPausa : MonoBehaviour
@@ -15,5 +16,10 @@ public class mettiInPausa : MonoBehaviour
         Time.timeScale=0f;
         pulsanteRazzo.interactable=false;
         transizione.SetTrigger("apriPausa");
+
+        /*solo nel tutorial la pausa interrompe anche il suono di scrittura*/
+        if(SceneManager.GetActiveScene().name=="tutorial") {
+            SManager.stopScrittura();
+        }
     }
 }
